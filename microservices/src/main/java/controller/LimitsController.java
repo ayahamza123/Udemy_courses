2c9e932477c477f1decc5fae2e405acef890e49d
ayahamza123 <aya.hamza@certigon.nl>
2022-04-07 22:40:43 +0100

    init

---
+++ microservices/src/main/java/controller/LimitsController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import beans.Limits;
+import configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+    @Autowired
+    private Configuration configuration;
+
+    public LimitsController(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits(){
+        //return new Limits(configuration.getMinimum(),configuration.getMaximum());
+      return new Limits(1,100);
+    }
+}
