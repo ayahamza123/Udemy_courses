73da8bfe0bc18a6ea1e79fe99a9e3a80baa4e6d4
ayahamza123 <aya.hamza@certigon.nl>
2022-04-11 11:46:11 +0100

    Init commit with configurations currency app

---
+++ currency-exchanges-services/src/main/java/com/microservices/currencyexchangesservices/CurrencyExchangesServicesApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.currencyexchangesservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangesServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangesServicesApplication.class, args);
+	}
+
+}
---
+++ currency-exchanges-services/src/test/java/com/microservices/currencyexchangesservices/CurrencyExchangesServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.currencyexchangesservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangesServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
