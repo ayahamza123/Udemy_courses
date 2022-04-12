be0f223d37220f6a94b6d88c76539a3ede43dd86
ayahamza123 <aya.hamza@certigon.nl>
2022-04-12 01:59:33 +0100

    configuring the server to choose the instances

--- currency-exchanges-services/src/main/java/com/microservices/currencyexchangesservices/CurrencyExchangesServicesApplication.java
+++ currency-exchanges-services/src/main/java/com/microservices/currencyexchangesservices/CurrencyExchangesServicesApplication.java
@@ -2,6 +2,8 @@ package com.microservices.currencyexchangesservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
 public class CurrencyExchangesServicesApplication {
